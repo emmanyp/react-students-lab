bbd123e1c2b4fa424cadb7cc6ca7b61a284131a1
emma <emma.ronaldo7@gmail.com>
2021-12-06 21:38:36 -0500

    done

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import studentsArr from './Data.js';
+import Students from './Students';
+
+function App() {
+	const students = studentsArr.map((ele, index) => {
+		return (
+			<Students name={ele.name} scores={ele.scores} bio={ele.bio} key={index} />
+		);
+	});
+
+	return <div className='App'>{students}</div>;
+}
+
+export default App;
